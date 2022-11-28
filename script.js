fe965e1dbe33cb0243221aadd6c15486308ed1fe
keller-johnson <keller.s.johnson@gmail.com>
2022-11-27 18:30:46 -0800

    saving my progress have to go to work

---
+++ script.js
@@ -0,0 +1,4 @@
+const music = document.querySelector("audio");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
