02343431d179c7cc72779873eb2a0eb84bd824c3
keller-johnson <keller.s.johnson@gmail.com>
2022-11-28 02:11:12 -0800

    added play button functionality with JS

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
+
+//Check if Playing
+let isPlaying = false;
+
+//play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "pause");
+  music.play();
+}
+
+//pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("play", "play");
+  music.pause();
+}
+
+//play or pause eventlistener
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
